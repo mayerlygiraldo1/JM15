d2caa464db474cdb34195e5366ed97692bb3e3da
Mayerly Giraldo <mayerlygiraldocalderon@gmail.com>
2016-08-16 15:42:55 -0500

    Front Formulario Registrarse y Perfil

---
+++ JMLoc15/Models/JMLoc15Context.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace JMLoc15.Models
+{
+    public class JMLoc15Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public JMLoc15Context() : base("name=JMLoc15Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<JMLoc15.Models.Lines> Lines { get; set; }
+
+        public System.Data.Entity.DbSet<JMLoc15.Models.Users> Users { get; set; }
+
+        public System.Data.Entity.DbSet<JMLoc15.Models.UserLines> UserLines { get; set; }
+    }
+}
---
+++ JMLoc15/Models/UserLines.cs
@@ -0,0 +1,9 @@
+﻿namespace JMLoc15.Models
+{
+    public class UserLines
+    {
+        public string Id { get; set; }
+        public Users User { get; set; }
+        public Lines Line { get; set; }
+    }
+}
